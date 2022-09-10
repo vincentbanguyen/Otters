6aa9af43aaca6493ee09f6475b289811dc99f36f
Vincent Nguyen <vincentbanguyen@gmail.com>
2022-09-09 19:53:34 -0500

    Add Textbox send mesage functionality

--- Otters/GroupChatView.swift
+++ Otters/GroupChatView.swift
@@ -1,8 +1,11 @@
 import SwiftUI
+import UIKit
 
 struct GroupChatView: View {
     @StateObject var groupChatVM = GroupChatViewModel()
     @State var otterUser = 0
+    @State var message = ""
+    @FocusState private var textFieldIsFocused: Bool
     var body: some View {
         VStack {
             Text(String(otterUser))
@@ -14,15 +17,55 @@ struct GroupChatView: View {
                         await groupChatVM.activateChatListeners()
                     }
                 }
-    
-            Text("send message")
-                .onTapGesture {
-                    groupChatVM.sendMessage(content: "HELLOOOOO")
+
+            HStack { // Message View
+                ZStack(alignment: .leading) {
+                    if message.isEmpty {
+                        Text("Message...").opacity(0.5)
+                    }
+                    TextField("", text: $message, onCommit: onCommit)
+                        .frame(width: 300, height: 40)
+                        .accentColor(.yellow)
+                        .disableAutocorrection(true)
+                        .focused($textFieldIsFocused)
                 }
+                
+                Button {
+                    onCommit()
+                } label: {
+                    Image(systemName: "paperplane.fill")
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .background(Color(.brown))
+                        .cornerRadius(50)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 10)
+            .background(Color(.gray))
+            .cornerRadius(50)
+            .padding()
+            
+            
             Text("Change user")
                 .onTapGesture {
                     otterUser += 1
                 }
         }
+    }
+    
+    func onCommit() -> Void {
+        if !message.isEmpty {
+            textFieldIsFocused = false
+            print("PROGRESS: Attempting to send: \(message)")
+            groupChatVM.sendMessage(content: message)
+            message = ""
+        }
+    }
+}
+
+struct GroupChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupChatView()
     }
 }
---
+++ Otters/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Otters
+//
+//  Created by Vincent Nguyen on 9/9/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
